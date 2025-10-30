144f762ed87b9dfa63b0c65e062d0c0d5478fdc6
nelifzyh <nelifauziyah07@gmail.com>
2025-10-30 16:59:09 +0700

    add command rcv on provider

--- app/Providers/ConsoleServiceProvider.php
+++ app/Providers/ConsoleServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Console\Commands\SyncEpicorPartTran; 
 use App\Console\Commands\SyncLaborDtl;
+use App\Console\Commands\SyncRcvDtl;
 
 class ConsoleServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,7 @@ class ConsoleServiceProvider extends ServiceProvider
     protected $commands = [
         SyncEpicorPartTran::class,
         SyncLaborDtl::class,
+        SyncRcvDtl::class,
     ];
 
     /**
