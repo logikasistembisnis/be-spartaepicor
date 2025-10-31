e00692d75a461d5da581e8f88ddf3cebee0ce1ce
nelifzyh <nelifauziyah07@gmail.com>
2025-10-31 10:48:34 +0700

    route warehouse

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ $router->get('/fetchdatalabordtl', 'LaborDtlController@fetchDataLaborDtl');
 $router->get('/fetchdatarcvdtl', 'RcvDtlController@fetchDataRcvDtl');
 $router->get('/fetchdataud06', 'UD06Controller@fetchDataUD06');
 $router->get('/fetchdatapart', 'PartController@fetchDataPart');
+$router->get('/fetchdatawarehouse', 'WarehouseController@fetchDataWarehouse');
