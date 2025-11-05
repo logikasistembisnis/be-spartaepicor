0c5a039436f25425385b86de911d34986b504fb6
nelifzyh <nelifauziyah07@gmail.com>
2025-11-05 16:20:22 +0700

    update

--- app/Console/Commands/SyncJobMtl.php
+++ app/Console/Commands/SyncJobMtl.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Http\Controllers\JobMtlController;
 use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
 class SyncJobMtl extends Command
 {
